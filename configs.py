f3cf8839c57b4699f5421c629b0166f8185129bb
julio.leao <julio.cleao@telefonica.com>
2025-04-08 05:19:45 -0300

    Adiciona dataset full

--- configs.py
+++ configs.py
@@ -2,12 +2,12 @@ import os
 
 import torch
 
-
+DATASET_TYPE = "knives-full"
 VIDEO_FILE = "video1.mp4"
 OUTPUT_VIDEO_FILE = f"output_{VIDEO_FILE}"
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 VIDEO_PATH = os.path.join("videos", VIDEO_FILE)
-KNIVES_YOLO_PATH = os.path.join(BASE_DIR, "dataset", "knives-yolo")
+KNIVES_YOLO_PATH = os.path.join(BASE_DIR, "dataset", DATASET_TYPE)
 DATA_PATH = os.path.join(KNIVES_YOLO_PATH, "data.yaml")
 TRAIN_PATH = os.path.join(KNIVES_YOLO_PATH, "train", "images")
 VAL_PATH = os.path.join(KNIVES_YOLO_PATH, "valid", "images")
